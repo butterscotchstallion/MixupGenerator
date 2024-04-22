3ba97045e8606ee4b41bfdfd1d416db36e5dbd11
Butterscotch Stallion <700364+butterscotchstallion@users.noreply.github.com>
2024-04-21 22:50:18 -0400

    Updates models/schemas and adds main API file

--- mixup_generator/api/schemas.py
+++ mixup_generator/api/schemas.py
@@ -2,6 +2,38 @@ from pydantic import BaseModel, ConfigDict
 from sqlalchemy import DateTime
 
 """
+team
+"""
+
+
+class Team(BaseModel):
+    id: int
+    created_at: DateTime
+    updated_at: DateTime
+    name: str
+
+
+class TeamMember(BaseModel):
+    id: int
+    name: str
+    created_at: DateTime
+    updated_at: DateTime
+
+
+"""
+meeting
+"""
+
+
+class Meeting(BaseModel):
+    id: int
+    meeting_timestamp: DateTime
+    created_at: DateTime
+    updated_at: DateTime
+    attendees: list[TeamMember]
+
+
+"""
 meeting_attendee
 """
 
@@ -13,30 +45,3 @@ class MeetingAttendee(BaseModel):
     updated_at: DateTime
     meeting_id: int
     team_member_id: int
-
-
-"""
-meeting
-"""
-
-
-class MeetingBase(BaseModel):
-    meeting_timestamp: DateTime
-    created_at: DateTime
-    updated_at: DateTime
-
-
-class Meeting(MeetingBase):
-    model_config = ConfigDict(from_attributes=True)
-    id: int
-    attendees: list[MeetingAttendee]
-
-
-"""
-team
-"""
-
-
-class Team(BaseModel):
-    created_at: DateTime
-    updated_at: DateTime
