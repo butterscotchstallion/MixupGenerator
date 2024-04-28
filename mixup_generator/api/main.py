f594fce26a1a331022039ac49b647abba8ac1ee2
Butterscotch Stallion <700364+butterscotchstallion@users.noreply.github.com>
2024-04-27 20:08:43 -0400

    Refactors to use SQLModel

--- mixup_generator/api/main.py
+++ mixup_generator/api/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 
+from mixup_generator.api.database import Database
+from mixup_generator.team import Team
+
 app = FastAPI()
 
+db = Database()
+engine = db.engine
+base = db.base
+base.metadata.create_all(bind=engine)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello world!"}
+
+@app.get("/teams")
+async def teams_list():
+    return {"teams": Team().query.all()}
--- mixup_generator/api/models.py
+++ mixup_generator/api/models.py
@@ -1,81 +1,127 @@
-from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String, func
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql import expression
+from datetime import datetime
+from typing import Optional
 
-from .db import Base
+from sqlalchemy import Boolean, Column, DateTime, String
+from sqlmodel import TIMESTAMP, Field, Relationship, SQLModel, text
 
 
-class Meeting(Base):
+class TeamMember(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    created_at: Optional[datetime] = Field(
+        sa_column=Column(
+            TIMESTAMP(timezone=True),
+            nullable=False,
+            server_default=text("CURRENT_TIMESTAMP"),
+        )
+    )
+    updated_at: Optional[datetime] = Field(
+        default_factory=datetime.utcnow,
+        sa_column_kwargs={"onupdate": datetime.utcnow},
+    )
+    name: str = Field(String, unique=True, max_length=50)
+    keycloak_user_id: str = Field(String, max_length=10, unique=True)
+    active: bool = Field(sa_column=Column(Boolean), default=True)
+    can_attend_multiple_meetings: bool = Field(sa_column=Column(Boolean), default=True)
+
+
+class Team(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    created_at: Optional[datetime] = Field(
+        sa_column=Column(
+            TIMESTAMP(timezone=True),
+            nullable=False,
+            server_default=text("CURRENT_TIMESTAMP"),
+        )
+    )
+    updated_at: Optional[datetime] = Field(
+        default_factory=datetime.utcnow,
+        sa_column_kwargs={"onupdate": datetime.utcnow},
+    )
+    name: str = Field(String, unique=True, max_length=50)
+    active: bool = Field(sa_column=Column(Boolean), default=True)
+
+
+class Meeting(SQLModel, table=True):
     """
     Represents a Mixup Meeting between two or more members
     """
 
-    __tablename__ = "meeting"
-    id = Column(Integer, primary_key=True)
-    meeting_timestamp = Column(DateTime(timezone=True))
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
-
-    attendees = relationship("MeetingAttendee", back_populates="attendees")
-    meeting_location_id = relationship(
-        "MeetingLocation", back_populates="meeting_location"
+    id: Optional[int] = Field(default=None, primary_key=True)
+    meeting_timestamp: DateTime = Field(DateTime(timezone=True))
+    created_at: Optional[datetime] = Field(
+        sa_column=Column(
+            TIMESTAMP(timezone=True),
+            nullable=False,
+            server_default=text("CURRENT_TIMESTAMP"),
+        )
+    )
+    updated_at: Optional[datetime] = Field(
+        default_factory=datetime.utcnow,
+        sa_column_kwargs={"onupdate": datetime.utcnow},
     )
 
+    attendees: list[TeamMember] = Relationship(back_populates="team")
+    meeting_location_id: int = Field(foreign_key="meeting_location._id")
 
-class MeetingLocation(Base):
+
+class MeetingLocation(SQLModel, table=True):
     """
     Represents a meeting room
     """
 
-    __tablename__ = "meeting_location"
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    id: Optional[int] = Field(default=None, primary_key=True)
+    created_at: Optional[datetime] = Field(
+        sa_column=Column(
+            TIMESTAMP(timezone=True),
+            nullable=False,
+            server_default=text("CURRENT_TIMESTAMP"),
+        )
+    )
+    updated_at: Optional[datetime] = Field(
+        default_factory=datetime.utcnow,
+        sa_column_kwargs={"onupdate": datetime.utcnow},
+    )
+    name: str = Field(String, unique=True, max_length=50)
 
 
-class MeetingAttendee(Base):
+class MeetingAttendee(SQLModel, table=True):
     """
     Each meeting can have a variable number of attendees and they are
     linked to the meeting by the mixup_meeting id
     """
 
-    __tablename__ = "meeting_attendee"
-    id = Column(Integer, primary_key=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
-    meeting_id = Column(Integer, ForeignKey("meeting.id"))
-    team_member_id = Column(Integer, ForeignKey("team_member.id"))
+    id: Optional[int] = Field(default=None, primary_key=True)
+    created_at: Optional[datetime] = Field(
+        sa_column=Column(
+            TIMESTAMP(timezone=True),
+            nullable=False,
+            server_default=text("CURRENT_TIMESTAMP"),
+        )
+    )
+    updated_at: Optional[datetime] = Field(
+        default_factory=datetime.utcnow,
+        sa_column_kwargs={"onupdate": datetime.utcnow},
+    )
+    meeting_id: int = Field(foreign_key="meeting.id")
+    team_member_id: int = Field(foreign_key="team_member.id")
 
 
-class TeamMember(Base):
-    __tablename__ = "team_member"
-    id = Column(Integer, primary_key=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
-    name = Column(String, unique=True)
-    keycloak_user_id = Column(String, unique=True)
-    active = Column(Boolean, server_default=expression.false())
-    can_attend_multiple_meetings = Column(Boolean, server_default=expression.false())
-
-
-class Team(Base):
-    __tablename__ = "team"
-    id = Column(Integer, primary_key=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
-    name = Column(String, unique=True)
-    active = Column(Boolean, server_default=expression.true())
-
-
-class TeamTeamMembers(Base):
+class TeamTeamMembers(SQLModel, table=True):
     """
     Represents team members belonging to one or more team
     """
 
-    __tablename__ = "team_team_member"
-    id = Column(Integer, primary_key=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
-    team_member_id = Column(Integer, ForeignKey("team_member.id"))
-    team_id = Column(Integer, ForeignKey("team.id"))
+    id: Optional[int] = Field(default=None, primary_key=True)
+    created_at: Optional[datetime] = Field(
+        sa_column=Column(
+            TIMESTAMP(timezone=True),
+            nullable=False,
+            server_default=text("CURRENT_TIMESTAMP"),
+        )
+    )
+    updated_at: Optional[datetime] = Field(
+        default_factory=datetime.utcnow,
+        sa_column_kwargs={"onupdate": datetime.utcnow},
+    )
+    team_member_id: int = Field(foreign_key="team_member.id")
+    team_id: int = Field(foreign_key="team.id")
