606064e730d3596a9701d59fb99aa75a25ee4d10
Butterscotch Stallion <700364+butterscotchstallion@users.noreply.github.com>
2024-05-06 23:40:18 -0400

    Implements various minor refactors
    - Adds more team members and associated data
    - Adds Prettier configuration options
    - Sorts team member list ascending

--- ui/MixupGenerator/src/routes/+page.svelte
+++ ui/MixupGenerator/src/routes/+page.svelte
@@ -14,20 +14,20 @@
 			getTeamsData$().subscribe(async (responses) => {
 				const teams: ITeam[] = [];
 				const teamMembersResponse: ITeamMember[] = JSON.parse(
-					await responses[2].text()
+					await responses[2].text(),
 				).team_members;
 				const teamsResponse: ITeamResponse[] = JSON.parse(await responses[0].text()).teams;
 				const teamMembersLinksResponse: ITeamMemberLinkResponse[] = JSON.parse(
-					await responses[1].text()
+					await responses[1].text(),
 				).team_member_team_link;
-				const teamToTeamMembersMap: Map<number, ITeamMember[]> = new Map<number, ITeamMember[]>();
-				const teamMemberIdToTeamMemberMap: Map<number, ITeamMember> = new Map<
+				const teamIdToTeamMembersMap: Map<number, ITeamMember[]> = new Map<number, ITeamMember[]>();
+				const teamMemberIdToTeamMembersMap: Map<number, ITeamMember> = new Map<
 					number,
 					ITeamMember
 				>();
 
 				teamMembersResponse?.forEach((teamMember: ITeamMember) => {
-					teamMemberIdToTeamMemberMap.set(teamMember.id, teamMember);
+					teamMemberIdToTeamMembersMap.set(teamMember.id, teamMember);
 				});
 
 				/**
@@ -40,8 +40,8 @@
 				 *
 				 */
 				teamMembersLinksResponse.forEach((teamMembers: ITeamMemberLinkResponse) => {
-					const existingTeamMembers = teamToTeamMembersMap.get(teamMembers.team_id);
-					const teamMember = teamMemberIdToTeamMemberMap.get(teamMembers.team_member_id);
+					const existingTeamMembers = teamIdToTeamMembersMap.get(teamMembers.team_id);
+					const teamMember = teamMemberIdToTeamMembersMap.get(teamMembers.team_member_id);
 
 					if (teamMember) {
 						let members: ITeamMember[] = [teamMember];
@@ -50,19 +50,19 @@
 							members = [...existingTeamMembers, teamMember];
 						}
 
-						teamToTeamMembersMap.set(teamMembers.team_id, members);
+						teamIdToTeamMembersMap.set(teamMembers.team_id, members);
 					}
 				});
 
 				teamsResponse.forEach((team: ITeam) => {
-					const members: ITeamMember[] = teamToTeamMembersMap.get(team.id) || [];
-					team.memberNames = members.map((m) => m.name).join(', ');
+					const members: ITeamMember[] = teamIdToTeamMembersMap.get(team.id) || [];
+					members.sort((a, b) => a.name.localeCompare(b.name));
 					team.members = members;
 					teams.push(team);
 				});
 
 				teams$.next(teams);
-			})
+			}),
 		);
 	}
 
@@ -70,7 +70,7 @@
 		return forkJoin([
 			fetch('http://127.0.0.1:8000/api/teams'),
 			fetch('http://127.0.0.1:8000/api/team-member-team-link'),
-			fetch('http://127.0.0.1:8000/api/team-members')
+			fetch('http://127.0.0.1:8000/api/team-members'),
 		]);
 	}
 
@@ -83,8 +83,8 @@
 	});
 </script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="grid grid-cols-1 gap-4">
+<div class="container h-full mx-auto flex justify-center">
+	<div class="grid grid-flow-row auto-rows-max">
 		<header class="card-header">
 			<h1 class="h1">
 				<span
@@ -116,8 +116,17 @@
 								<tr>
 									<td>{team.name}</td>
 									<td>
-										{#if team?.members?.length > 0}
-											{team.memberNames}
+										{#if team?.members && team.members.length > 0}
+											<ul>
+												{#each team?.members as member}
+													<li>
+														<a
+															class="anchor"
+															href="#modal">{member.name}</a
+														>
+													</li>
+												{/each}
+											</ul>
 										{/if}
 									</td>
 								</tr>
